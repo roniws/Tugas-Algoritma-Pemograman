fb391e46d18cd5cb89cd09b93881608feff5e64f
roniws <roni@gerbangdigital.com>
2021-12-23 18:01:58 +0700

    Tugas Algoritma Pemograman

--- MenampilkanDataMahasiswa.py
+++ MenampilkanDataMahasiswa.py
@@ -1,37 +1,32 @@
-mhs1 = {
-    'nama':'roni wahyudi setiawan',
-    'nim':7120210042,
-    'thn_lahir':1988,
-}
+from datetime import date
 
-mhs2 = {
-    'nama':'joni dalbo',
-    'nim':4120232,
-    'thn_lahir':1989
-}
-mhs3 = {
-    'nama':'johan prastyo',
-    'nim':4120233,
-    'thn_lahir':1990
-}
-mhs4 = {
-    'nama':'Dini',
-    'nim':4120234,
-    'thn_lahir':1991
-}
-mhs5 = {
-    'nama':'Andika',
-    'nim':4120235,
-    'thn_lahir':1992
-}
-
-usia1 = 2021 - mhs1['thn_lahir']
-usia2 = 2021 - mhs2['thn_lahir']
-usia3 = 2021 - mhs3['thn_lahir']
-usia4 = 2021 - mhs4['thn_lahir']
-usia5 = 2021 - mhs5['thn_lahir']
-print(f"Mahasiswa ke 1\nNAMA : {mhs1['nama']}\nNIM :{mhs1['nim']}\nTAHUN LAHIR : {mhs1['thn_lahir']}\nUSIA : {usia1}")
-print(f"Mahasiswa ke 2\nNAMA : {mhs2['nama']}\nNIM :{mhs2['nim']}\nTAHUN LAHIR : {mhs2['thn_lahir']}\nUSIA : {usia2}")
-print(f"Mahasiswa ke 3\nNAMA : {mhs3['nama']}\nNIM :{mhs3['nim']}\nTAHUN LAHIR : {mhs3['thn_lahir']}\nUSIA : {usia3}")
-print(f"Mahasiswa ke 4\nNAMA : {mhs4['nama']}\nNIM :{mhs4['nim']}\nTAHUN LAHIR : {mhs4['thn_lahir']}\nUSIA : {usia4}")
-print(f"Mahasiswa ke 5\nNAMA : {mhs5['nama']}\nNIM :{mhs5['nim']}\nTAHUN LAHIR : {mhs5['thn_lahir']}\nUSIA : {usia5}")
+dataNama = []
+dataNim = []
+dataThnLahir = []
+stop = False
+foo = 0
+class dataMahasiswa:
+    def __init__(self,nama,nim,ThnLahir):
+        self.nama = nama
+        dataNama.append(nama)
+        self.nim = nim
+        dataNim.append(nim)
+        self.ThnLahir = ThnLahir
+        dataThnLahir.append(ThnLahir)
+    def tampilMhs():
+        n = 0
+        for i in range (foo):
+            n+=1
+            hari = date.today()
+            umur = hari.year - dataThnLahir[i]
+            print(f'mahasiswa ke{n}......\nNama = {dataNama[i]}\nNIM = {dataNim[i]}\nTahun Lahir = {dataThnLahir[i]}\nUmur = {umur}')
+while(not stop):
+    inputNama = input('masukkan nama mahasiswa = ')
+    inputNim = input('masukkan NIM = ')
+    inputThnLahir = int(input('masukkan Tahun Lahir = '))
+    mahasiswa = dataMahasiswa(inputNama,inputNim,inputThnLahir)
+    ulang = input('apakah ingin input ulang (y/t) = ')
+    foo +=1
+    if ulang == 't':
+        stop = True
+dataMahasiswa.tampilMhs()
